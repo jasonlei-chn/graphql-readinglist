f03d2fb3230440477f81a1f4f4bcb4e5682aceb6
Jiayi Lei <leij@fb.com>
2020-10-09 20:09:44 -0700

    first commit

---
+++ server/schema/schema.js
@@ -0,0 +1,53 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString } = graphql;
+
+// dummy data
+let books = [
+  {
+    id: 1,
+    name: "Name of the Wind",
+    genere: "Fantasy",
+  },
+  {
+    id: 2,
+    name: "The Final Empire",
+    genere: "Fantasy",
+  },
+  {
+    id: 3,
+    name: "The Long Earth",
+    genere: "Sci-Fi",
+  },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "BookType description",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    genere: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  description: "RootQueryType description",
+  fields: {
+    book: {
+      type: BookType,
+      description: "Given an id(int) of the book, query its id, name, genere",
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        // code to get data from db/other source
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
