b7632f5e454dba1ca4246d20dc76917b42009b8f
Jiayi Lei <leij@fb.com>
2020-10-10 15:07:01 -0700

    tutorial #24 - BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+
+// components
+import BookList from "./componenets/BookList";
 
 function App() {
   return (
     <div id="main">
       <h1>Jiayi's Reading List</h1>
+      <BookList />
     </div>
   );
 }
