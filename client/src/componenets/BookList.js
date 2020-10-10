a9890e538cc60d46449407ebcb956ba84240664a
Jiayi Lei <leij@fb.com>
2020-10-10 15:58:03 -0700

    tutorial #26 - Making queries from React

--- client/src/componenets/BookList.js
+++ client/src/componenets/BookList.js
@@ -1,15 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
-class BookList extends Component {
-  render() {
-    return (
-      <div>
-        <ul id="book-list">
-          <li>Book Name</li>
-        </ul>
-      </div>
-    );
+const getBookQuery = gql`
+  {
+    books {
+      name
+      genere
+    }
   }
+`;
+
+function BookList() {
+  const { loading, error, data } = useQuery(getBookQuery);
+  console.log(data);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Ops! Something went wrong!</p>;
+  }
+  return (
+    <div>
+      <h1>All good! got the data!!</h1>
+    </div>
+  );
 }
 
 export default BookList;
