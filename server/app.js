00261244ebec6dfbb918c43114d94b2733817a9b
Jiayi Lei <leij@fb.com>
2020-10-09 22:02:55 -0700

    tutorial #16 - Connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://admin:jNflZsq86dHQkUwK@cluster0.zdiou.mongodb.net/Cluster0?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to MongoDB");
+});
 
 app.use(
   "/graphql",
