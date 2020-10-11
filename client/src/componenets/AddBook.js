98910b7b441b8de4d78a55e536adf32596ee38fc
Jiayi Lei <leij@fb.com>
2020-10-10 19:33:34 -0700

    put useQuery back to its component

--- client/src/componenets/AddBook.js
+++ client/src/componenets/AddBook.js
@@ -1,7 +1,25 @@
 import React from "react";
-import DiaplayAuthors from './DiaplayAuthors';
+import { useQuery } from "@apollo/client";
+import { GET_AUTHORS } from "../queries/queries";
+
+const displayAuthors = (loading, error, data) => {
+  if (loading) {
+    return <option disabled>Loading Authors...</option>;
+  }
+  if (error) {
+    return <option disabled>Failed to load authors!</option>;
+  }
+  return data.authors.map((author) => {
+    return (
+      <option key={author.id} value={author.id}>
+        {author.name}
+      </option>
+    );
+  });
+};
 
 function AddBooks() {
+  const { loading, error, data } = useQuery(GET_AUTHORS);
   return (
     <form id="add-book">
       <div className="field">
@@ -16,7 +34,7 @@ function AddBooks() {
         <label>Author:</label>
         <select>
           <option>Select author</option>
-          <DiaplayAuthors />
+          {displayAuthors(loading, error, data)}
         </select>
       </div>
       <button>+</button>
--- client/src/componenets/BookList.js
+++ client/src/componenets/BookList.js
@@ -1,12 +1,24 @@
 import React from "react";
-import DisplayBooks from "./DisplayBooks";
+import { useQuery } from "@apollo/client";
+import { GET_BOOKS } from "../queries/queries";
+
+const displayBooks = (loading, error, data) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>`Error! ${error.message}`</p>;
+  }
+  return data.books.map((book) => {
+    return <li key={book.id}>{book.name}</li>;
+  });
+};
 
 function BookList() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
   return (
     <div>
-      <ul id="book-list">
-        <DisplayBooks />
-      </ul>
+      <ul id="book-list">{displayBooks(loading, error, data)}</ul>
     </div>
   );
 }
