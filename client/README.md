aa76436b22ffd767160f2afad57f72902db5f479
Jiayi Lei <leij@fb.com>
2020-10-11 11:00:15 -0700

    tutorial #26 - making queries from React

--- client/README.md
+++ client/README.md
@@ -72,5 +72,5 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/tr
 
 Dependencies:
 ```
-npm install apollo-boost react-apollo graphql --save
+npm install apollo-boost react-apollo graphql cors --save
 ```
--- server/app.js
+++ server/app.js
@@ -3,16 +3,19 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
-
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
+// allow cross-origin requests
+app.use(cors());
+
+// connect to MongoDB
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const db_name = process.env.DB_NAME;
 const db_uri = `mongodb+srv://${username}:${password}@cluster0.zdiou.mongodb.net/${db_name}?retryWrites=true&w=majority`;
-
 mongoose.connect(db_uri);
 mongoose.connection.once("open", () => {
   console.log(chalk.green("[APP] Connected to MongoDB"));
